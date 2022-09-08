617250c9961a250bf47aba6d8eb9ab7574ecc819
evgeny kuraev <evgeny.kuraev@gmail.com>
2022-09-08 21:17:56 +0300

    HW2_3

---
+++ HW2/HW2_3/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Введите день недели: ");
+int number= Convert.ToInt32(Console.ReadLine());
+if (number == 6 || number == 7) Console.WriteLine("это выходной");
+else Console.WriteLine("это не выходной");
+
