77c11af1a30453dae8ba5145081f23c6b2aae11c
evgeny kuraev <evgeny.kuraev@gmail.com>
2022-09-08 21:05:32 +0300

    HW2_1

---
+++ HW2/HW2_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int thirdDigit=N/10%10;
+
+Console.WriteLine($"Вторая цифра - {thirdDigit}");
+
