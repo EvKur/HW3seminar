a160b8721f3dac55158ee92da974bcc7c4b1d575
evgeny kuraev <evgeny.kuraev@gmail.com>
2022-09-08 19:49:58 +0300

    HW3_2

---
+++ HW3/HW3_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int firstDigit=N/10000;
+int fifthDigit=N%10;
+int secondDigit=N/1000%10;
+int fourthDigit=N/10%10;
+
+if (firstDigit==fifthDigit&&secondDigit==fourthDigit) Console.WriteLine("Введенное число являтся палиндромом");
+else Console.WriteLine("Введенное число не являтся палиндромом");
