ad7777215b74815c7c72c280d2e4692a0ad3f65e
evgeny kuraev <evgeny.kuraev@gmail.com>
2022-09-15 15:48:43 +0300

    27a

---
+++ HW4/27a/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(GetSumNumbers(number));
+int GetSumNumbers(int num)
+{
+    int sum = 0;
+    int next = 0;
+    while (num >0)
+    {
+      next = num - num%10;
+      sum= sum + (num - next);
+      num=num/10; 
+    }
+    return sum;
+}
+
