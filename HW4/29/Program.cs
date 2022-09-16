a84133278bbed0803a5fa8759f902542f9aa4ed6
evgeny kuraev <evgeny.kuraev@gmail.com>
2022-09-16 16:49:28 +0300

    29

---
+++ HW4/29/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+
+int[] array=new int[8];
+
+for (int i=0;i<array.Length;i++)
+{
+   
+    array[i]= new Random().Next(); //or array[i]=Convert.ToInt32(Console.ReadLine());
+
+}
+Console.WriteLine($"Result array: [ {String.Join(", ", array)} ]");
