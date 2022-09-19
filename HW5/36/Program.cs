5bdb3c3d5e023d8be242d1020db61593d5d93068
evgeny kuraev <evgeny.kuraev@gmail.com>
2022-09-19 17:29:45 +0300

    36

---
+++ HW5/36/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+int size=6;
+int[] array=new int[size];
+int SumElementsWithOddIndex=0;
+
+for(int i=0;i<size;i++){
+    array[i]=new Random().Next(20);
+    Console.WriteLine(array[i]+"\t");
+    if(i%2!=0){
+        SumElementsWithOddIndex+=array[i];}
+}
+Console.WriteLine($"Сумма элементов стоящих на нечетных индексах равна: {SumElementsWithOddIndex}");
