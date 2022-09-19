85b617356fdbc8adff7fdfc9464027b32022596c
evgeny kuraev <evgeny.kuraev@gmail.com>
2022-09-19 15:34:08 +0300

    34

---
+++ HW5/34/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//[345, 897, 568, 234] -> 2
+
+int size=10;
+
+int[] array = new int[size];
+int CountEvenNumbersInArray=0;
+for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+        Console.Write(array[i] + "\t");
+        if (array[i]%2 == 0) CountEvenNumbersInArray++;
+    }
+Console.WriteLine();
+Console.WriteLine($"Количество четных чисел в заданном массиве - {CountEvenNumbersInArray}");
